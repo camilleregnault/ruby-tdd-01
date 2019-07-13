d35f7d74fe389dd046eeb37792a8917ea210c01f
Camille Regnault <c.regnault.pro@gmail.com>
2019-07-13 12:49:50 +0200

    rename files

---
+++ FizzBuzz/fizz_buzz_spec.rb
@@ -0,0 +1,5 @@
+# it takes an integer as an input.
+# it returns an array which contains a serie of integer from 1 to the integer input.
+# in the output array, any multiple of 3 is replaced by 'Fizz', any multiple of 5 is replaced by 'Buzz' and any multiple of both 3 and 5 is replaced by 'FizzBuzz'.
+# - what happens if the input is not an integer? - what happens if the input is negative?
+
